938b49c47b8c2e7fe35e21057f96a9ce59c6920b
Netbook-James <James-Timms@hotmail.com>
2015-12-02 15:28:40 +0000

    Added a default Dashboard Bootstrap webpage to build apon. Frontend is looking nicer now.

--- app/app.js
+++ app/app.js
@@ -15,15 +15,15 @@ var ImportIoQuery = function(){
         + "&_apikey=" + ipc.sendSync("get_api_key");
 };
 
-document.write("\<webview id=\"data_page\" src=\"" + ImportIoQuery()
-    + "\" style=\"display:inline-block; width:640px; height:480px\" nodeintegration\>\</webview\>");
-
-onload = function() {
-    var webview = document.getElementById('data_page');
-    var loadstop = function() {
-        var js = "var ipc = require('ipc'); ipc.send('get-data', document.body.innerText);";
-        webview.executeJavaScript(js, false);
-    };
-
-    webview.addEventListener("did-stop-loading", loadstop);
-};
+//document.write("\<webview id=\"data_page\" src=\"" + ImportIoQuery()
+//    + "\" style=\"display:inline-block; width:640px; height:480px\" nodeintegration\>\</webview\>");
+//
+//onload = function() {
+//    var webview = document.getElementById('data_page');
+//    var loadstop = function() {
+//        var js = "var ipc = require('ipc'); ipc.send('get-data', document.body.innerText);";
+//        webview.executeJavaScript(js, false);
+//    };
+//
+//    webview.addEventListener("did-stop-loading", loadstop);
+//};
