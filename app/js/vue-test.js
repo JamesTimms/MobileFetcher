c07f186ff772b0ae061b7a4e4ec96bb72a694044
Netbook-James <James-Timms@hotmail.com>
2015-12-08 12:07:32 +0000

    Added in vue.js for the front end. It's a reactive frontend that follows Model-View-View-Model. Javascript that controls the DOM.

---
+++ app/js/vue-test.js
@@ -0,0 +1,23 @@
+/**
+ * Created by James on 08/12/2015.
+ */
+
+new Vue({
+    el: '#app',
+    data: {
+        devices: [
+            {
+                name: 'iPhone6',
+                ipv6: 'yes'
+            },
+            {
+                name: 'Samsung S6',
+                ipv6: 'yes'
+            },
+            {
+                name: 'EE Harrior',
+                ipv6: '?'
+            }
+        ]
+    }
+});
