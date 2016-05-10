34b3dcb8846815b39a493c0c1b1242e49fa92c7b
Netbook-James <James-Timms@hotmail.com>
2016-05-10 14:44:24 +0100

    Finished the unit tests for the JSON file exporter.

--- app/js/data-parser.js
+++ app/js/data-parser.js
@@ -2,49 +2,54 @@
  * Created by James on 30/11/2015.
  */
 'use strict';
-
+//
 var fs = require('fs');
 var csv = require('csv');
+var fileWriteStream = '';
 
-var DataParser = function (file, data, callback) {
-    //if (isArray(data)) {
-    //    data.forEach(function (_d) {
-    //        deviceDataToFile(file, _d)
-    //    })
-    //} else {
-    //    deviceDataToFile(file, data);
-    //}
-    deviceDataToFile(file, data, callback);
+var translateDataFields = function(data) {
+    var _d = {};
+    _d[data['title']] = {
+        MARKETING_NAME: data['title'],
+        FORM_FACTOR: '',
+        DEVICE_TYPE: '',
+        NETWORK_TECH: data['network']['technology'],
+        OS: data['platform']['OS'],
+        UE_CATEGORY: '',
+        IPV6_FLAT: '',
+        VoWFi_FLAG: '',
+        VoLTE_FLAG: '',
+        SCREEN_SIZE: data['display']['resolution'], //Might need to review this value. More data available here.
+        LAUNCH_DATE: data['launch']['status'],
+        MAX_SPEED: data['technology'], //Will need to do some inference here. Regex to find strings etc...
+        CHIPSET: ''
+    };
+    return _d;
+}
+
+function DataParser(file, callback) {
+    fileWriteStream = fs.createWriteStream(file);
+    if (callback) {
+        fileWriteStream.on('finish', callback);
+    }
+    // fileWriteStream.write("{", 'utf8');
 };
 
-var deviceDataToFile = function (file, data, callback) {
+DataParser.prototype.deviceDataToFile = function(data, callback) {
+    if (fileWriteStream == '') {
+        console.log('File stream is closed. Cannot write to file.');
+        return;
+    }
     if (!data) {
         console.log('Failed to write to file because there was no data.');
         return;
     }
 
+    var _d = translateDataFields(data);
+    _d = JSON.stringify(_d);
+
     try {
-        fs.readFile(file, 'utf8', function (e, d) {
-            //console.log(d);
-            var _d = (d === '') ? {} : JSON.parse(d);
-            //console.log(_d);
-            _d[data['title']] = {
-                MARKETING_NAME: data['title'],
-                FORM_FACTOR: '',
-                DEVICE_TYPE: '',
-                NETWORK_TECH: data['network']['technology'],
-                OS: data['platform']['OS'],
-                UE_CATEGORY: '',
-                IPV6_FLAT: '',
-                VoWFi_FLAG: '',
-                VoLTE_FLAG: '',
-                SCREEN_SIZE: data['display']['resolution'],//Might need to review this value. More data available here.
-                LAUNCH_DATE: data['launch']['status'],
-                MAX_SPEED: data['technology'],//Will need to do some inference here. Regex to find strings etc...
-                CHIPSET: ''
-            };
-            fs.writeFile(file, JSON.stringify(_d), [], callback);
-        });
+        fileWriteStream.write(_d, 'utf8');
         //var csv_data = [["MARKETING_NAME", "FORM_FACTOR"], [data['title'], '']];
 
         //csv.generate({seed: 1, columns: 2, length: 20}, function(err, data){
@@ -62,7 +67,12 @@ var deviceDataToFile = function (file, data, callback) {
     }
 };
 
-var logError = function (file, data) {
+DataParser.prototype.finished = function() {
+    fileWriteStream.end(); // fileWriteStream.end("}", 'utf8');
+    fileWriteStream = '';
+}
+
+var logError = function(file, data) {
     if (!data) {
         console.log('Failed to write to file because data was null.');
         return;
@@ -80,3 +90,4 @@ var logError = function (file, data) {
 
 module.exports = DataParser;
 module.exports.logError = logError;
+module.exports.translateDataFields = translateDataFields;
